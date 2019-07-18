72222793cd3007a716c2dbccffc63c1426db49e9
malsushila <53028971+malsushila@users.noreply.github.com>
2019-07-18 09:22:56 +0530

    Create frame.c

---
+++ frame.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{   
+printf("welcome to horizon 2K19");
+printf("let do simple addition and subtraction code in c");
+int a,b,c,d;
+printf("enter the value of a");
+scanf("%d",&a);
+printf('enter the value of b");
+scanf("%d",&b);
+b=a-b
+b=a+b
+a=b-a
+}
